3529b60b31a004ff349ed09b25728561f384fa50
Seva <20211206nch@mail.ru>
2025-11-12 07:33:18 +0300

    feat: Input

---
+++ client/src/Input/Input.jsx
@@ -0,0 +1,83 @@
+import cn from './Input.module.css'
+
+export function Input({
+  value,
+  onChange,
+  placeholder,
+  containerClass = {},
+  showClear = false,
+  onClear,
+  ...props
+}) {
+  const containerStyle = {
+    width: containerClass.width,
+    marginRight: containerClass.marginRight,
+    position: 'relative',
+    display: 'inline-block',
+  }
+
+  return (
+    <div style={containerStyle}>
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={cn.input}
+        {...props}
+      />
+      {showClear && value && (
+        <button
+          type="button"
+          className={cn.clearButton}
+          onClick={onClear}
+          aria-label="Очистить"
+        >
+          ×
+        </button>
+      )}
+    </div>
+  )
+}
+
+// import { forwardRef } from 'react'
+
+// import styles from './Input.module.css'
+
+// export const Input = forwardRef(
+//   (
+//     {
+//       name = '',
+//       type = 'text',
+//       label = '',
+//       placeholder = '',
+//       error = '',
+//       useLabel = true,
+//       containerClass = '',
+//       ...props
+//     },
+//     ref
+//   ) => {
+//     const labelName = label || name
+
+//     return (
+//       <div className={`${(containerClass, styles.inputContainer)}`}>
+//         {useLabel && (
+//           <label htmlFor={name} className={styles?.label}>
+//             {labelName}
+//           </label>
+//         )}
+
+//         <input
+//           ref={ref}
+//           id={name}
+//           type={type}
+//           name={name}
+//           placeholder={placeholder || name}
+//           className={`${styles.input} ${error ? styles.error : ''}  ${props.disabled ? styles.disabled : ''}`}
+//           {...props}
+//         />
+//       </div>
+//     )
+//   }
+// )
