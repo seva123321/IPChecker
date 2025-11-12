188b9dc04ff9100e9d939f4c1de4954864ab05ef
Seva <20211206nch@mail.ru>
2025-11-12 07:29:18 +0300

    feat: routes.js

---
+++ client/src/routes.js
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  BASE_URL: 'http://localhost',
+  PORT: 5000,
+  IP: 'ip',
+  PORTS: 'ports',
+  KEYWORDS: 'keywords',
+  GROUP: '/group',
+  FILES: 'files',
+  // FILES: (pathname)=> `/files/${pathname}`,
+}
