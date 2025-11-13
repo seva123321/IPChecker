31b83d632e002a65b16c0dd95146979249aad55d
Seva <20211206nch@mail.ru>
2025-11-13 06:48:34 +0300

    fix: Apiservice worked with error.message

--- client/src/ApiService.js
+++ client/src/ApiService.js
@@ -34,73 +34,135 @@ export const createUrl = (pathname, params = {}) => {
   return url
 }
 
+// Универсальная функция обработки ошибок
+const handleApiError = (error) => {
+  // Обрабатываем ошибки и возвращаем сообщение из ответа сервера
+  if (error.response && error.response.data) {
+    // Если сервер вернул JSON с сообщением
+    if (error.response.data.message) {
+      return new Error(error.response.data.message)
+    }
+    // Если сервер вернул ошибку в другом формате
+    if (error.response.data.error) {
+      return new Error(error.response.data.error)
+    }
+  }
+  // Если нет данных от сервера, но есть сообщение об ошибке
+  if (error.message) {
+    return new Error(error.message)
+  }
+  return new Error('Произошла ошибка при запросе')
+}
+
+// Универсальная функция для вызова API с обработкой ошибок
+const callApi = async (method, pathname, params = {}, data = null) => {
+  let url
+
+  switch (method.toLowerCase()) {
+    case 'get':
+      url = createUrl(pathname, params)
+      console.log(`${method.toUpperCase()} - URL:`, url.toString())
+      return await axios.get(url.toString())
+    case 'post':
+      url = createUrl(pathname)
+      console.log(`${method.toUpperCase()} - URL:`, url.toString())
+      return await axios.post(url.toString(), data)
+    case 'put':
+      url = createUrl(pathname)
+      console.log(`${method.toUpperCase()} - URL:`, url.toString())
+      return await axios.put(url.toString(), data)
+    case 'delete':
+      url = createUrl(pathname, params)
+      console.log(`${method.toUpperCase()} - URL:`, url.toString())
+      return await axios.delete(url.toString())
+    default:
+      throw new Error(`Unsupported method: ${method}`)
+  }
+}
+
 export class ApiService {
   // Универсальный метод для вызова API
   static async call(method, pathname, params = {}, data = null) {
-    let url
-
-    switch (method.toLowerCase()) {
-      case 'get':
-        url = createUrl(pathname, params)
-        console.log(`${method.toUpperCase()} - URL:`, url.toString())
-        return await axios.get(url.toString())
-      case 'post':
-        url = createUrl(pathname)
-        console.log(`${method.toUpperCase()} - URL:`, url.toString())
-        return await axios.post(url.toString(), data)
-      case 'put':
-        url = createUrl(pathname)
-        console.log(`${method.toUpperCase()} - URL:`, url.toString())
-        return await axios.put(url.toString(), data)
-      case 'delete':
-        url = createUrl(pathname, params)
-        console.log(`${method.toUpperCase()} - URL:`, url.toString())
-        return await axios.delete(url.toString())
-      default:
-        throw new Error(`Unsupported method: ${method}`)
-    }
+    return await callApi(method, pathname, params, data)
   }
 
   static async getData(pathname, params) {
-    const data = await ApiService.call('get', pathname, params)
-    return data
+    try {
+      const response = await callApi('get', pathname, params)
+      return response.data
+    } catch (error) {
+      throw handleApiError(error)
+    }
+  }
+
+  static async postData(pathname, formData) {
+    try {
+      const response = await callApi('post', pathname, {}, formData)
+      return response.data
+    } catch (error) {
+      throw handleApiError(error)
+    }
   }
 
   static async searchData(pathname, params) {
-    const data = await ApiService.call('get', pathname, params)
-    return data
+    try {
+      const response = await callApi('get', pathname, params)
+      return response.data
+    } catch (error) {
+      throw handleApiError(error)
+    }
   }
 
   static async getFileJson(pathname) {
-    const data = await ApiService.call('get', pathname)
-    return data
+    try {
+      const response = await callApi('get', pathname)
+      return response.data
+    } catch (error) {
+      throw handleApiError(error)
+    }
   }
 
   static async uploadFiles(endpoint, formData) {
-    const response = await ApiService.call('post', endpoint, {}, formData)
-    return response
+    try {
+      const response = await callApi('post', endpoint, {}, formData)
+      return response.data
+    } catch (error) {
+      throw handleApiError(error)
+    }
   }
 
   //**************** */
   static async exportAll(pathname) {
-    const data = await ApiService.call('get', pathname)
-    return data
+    try {
+      const response = await callApi('get', pathname)
+      return response.data
+    } catch (error) {
+      throw handleApiError(error)
+    }
   }
 
   static async exportSession(limit = 100) {
-    const response = await ApiService.call('get', '/api/export/session', {
-      limit,
-    })
-    return response
+    try {
+      const response = await callApi('get', '/api/export/session', {
+        limit,
+      })
+      return response.data
+    } catch (error) {
+      throw handleApiError(error)
+    }
   }
 
   // Добавлен метод для экспорта по датам
   static async exportByDateRange(startDate, endDate) {
-    const response = await ApiService.call('get', '/files/daterange', {
-      startDate,
-      endDate,
-    })
-    return response
+    try {
+      const response = await callApi('get', '/files/daterange', {
+        startDate,
+        endDate,
+      })
+      return response.data
+    } catch (error) {
+      throw handleApiError(error)
+    }
   }
 
   //**************** */
@@ -108,8 +170,7 @@ export class ApiService {
 
 export const service = new ApiService()
 
-//**************** */
-// УНИВЕРСАЛЬНЫЙ
+// // ApiService.js
 // import axios from 'axios'
 // import { ROUTES } from './routes'
 
@@ -146,37 +207,54 @@ export const service = new ApiService()
 // }
 
 // export class ApiService {
-//   static async getData(pathname, params) {
+//   // Универсальный метод для вызова API
+//   static async call(method, pathname, params = {}, data = null) {
 //     let url
-//     if (params) {
-//       // Для поисковых запросов используем createUrl с параметрами
-//       url = createUrl(pathname, params)
-//       console.log('getData - search URL:', url.toString())
-//     } else {
-//       // Для обычных запросов используем createUrl
-//       url = createUrl(pathname)
-//       console.log('getData - data URL:', url.toString())
-//     }
 
-//     const data = await axios.get(url.toString())
+//     switch (method.toLowerCase()) {
+//       case 'get':
+//         url = createUrl(pathname, params)
+//         console.log(`${method.toUpperCase()} - URL:`, url.toString())
+//         return await axios.get(url.toString())
+//       case 'post':
+//         url = createUrl(pathname)
+//         console.log(`${method.toUpperCase()} - URL:`, url.toString())
+//         return await axios.post(url.toString(), data)
+//       case 'put':
+//         url = createUrl(pathname)
+//         console.log(`${method.toUpperCase()} - URL:`, url.toString())
+//         return await axios.put(url.toString(), data)
+//       case 'delete':
+//         url = createUrl(pathname, params)
+//         console.log(`${method.toUpperCase()} - URL:`, url.toString())
+//         return await axios.delete(url.toString())
+//       default:
+//         throw new Error(`Unsupported method: ${method}`)
+//     }
+//   }
+
+//   static async getData(pathname, params) {
+//     const data = await ApiService.call('get', pathname, params)
+//     return data
+//   }
+
+//   static async postData(pathname, formData) {
+//     const data = await ApiService.call('post', pathname, formData)
 //     return data
 //   }
 
 //   static async searchData(pathname, params) {
-//     const data = await ApiService.getData(pathname, params)
+//     const data = await ApiService.call('get', pathname, params)
 //     return data
 //   }
 
 //   static async getFileJson(pathname) {
-//     const data = await ApiService.getData(pathname)
+//     const data = await ApiService.call('get', pathname)
 //     return data
 //   }
 
 //   static async uploadFiles(endpoint, formData) {
-//     const url = createUrl(endpoint)
-//     const response = await axios.post(url.toString(), formData, {
-//       headers: { 'Content-Type': 'multipart/form-data' },
-//     })
+//     const response = await ApiService.call('post', endpoint, {}, formData)
 //     return response
 //   }
 
@@ -187,8 +265,9 @@ export const service = new ApiService()
 //   }
 
 //   static async exportSession(limit = 100) {
-//     const url = createUrl('/api/export/session', { limit })
-//     const response = await axios.get(url.toString())
+//     const response = await ApiService.call('get', '/api/export/session', {
+//       limit,
+//     })
 //     return response
 //   }
 
@@ -201,130 +280,6 @@ export const service = new ApiService()
 //     return response
 //   }
 
-//   static async exportRange(startIp, endIp) {
-//     const url = createUrl('/api/export/range', { startIp, endIp })
-//     const response = await axios.get(url.toString())
-//     return response
-//   }
-
-//   // Универсальный метод для вызова API
-//   static async call(method, pathname, params = {}, data = null) {
-//     let url
-
-//     switch (method.toLowerCase()) {
-//       case 'get':
-//         url = createUrl(pathname, params)
-//         return await axios.get(url.toString())
-//       case 'post':
-//         url = createUrl(pathname)
-//         return await axios.post(url.toString(), data)
-//       case 'put':
-//         url = createUrl(pathname)
-//         return await axios.put(url.toString(), data)
-//       case 'delete':
-//         url = createUrl(pathname, params)
-//         return await axios.delete(url.toString())
-//       default:
-//         throw new Error(`Unsupported method: ${method}`)
-//     }
-//   }
-//   //**************** */
-// }
-
-// export const service = new ApiService()
-
-//**************** */
-// УНИВЕРСАЛЬНЫЙ КОНЕЦ
-
-// import axios from 'axios'
-// import { ROUTES } from './routes'
-
-// export class ApiService {
-//   static createUrlData(pathname) {
-//     const baseUrl = `${ROUTES.BASE_URL}:${ROUTES.PORT}`
-//     const path = `${ROUTES[String(pathname).toUpperCase()]}`
-//     const url = new URL(path, baseUrl)
-//     return url
-//   }
-
-//   static createUrlSearch(pathname, { name, value }) {
-//     const baseUrl = `${ROUTES.BASE_URL}:${ROUTES.PORT}`
-//     const pathRes = `${ROUTES[String(pathname.split('/')[0]).toUpperCase()]}`
-//     const path =
-//       pathname.split('/')[1] === 'group' ? `${pathRes}/group` : pathRes
-
-//     const url = new URL(path, baseUrl)
-//     url.searchParams.set(name, value)
-//     return url
-//   }
-
-//   static async getData(pathname, params) {
-//     if (params) {
-//       const url = ApiService.createUrlSearch(pathname, params)
-//       console.log('getData - search URL:', url.toString())
-//       const data = await axios.get(url.toString())
-//       return data
-//     } else {
-//       const url = ApiService.createUrlData(pathname)
-//       console.log('getData - data URL:', url.toString())
-//       const data = await axios.get(url.toString())
-//       return data
-//     }
-//   }
-
-//   static async searchData(pathname, params) {
-//     const data = await ApiService.getData(pathname, params)
-//     return data
-//   }
-//   static async getFileJson(pathname) {
-//     const data = await ApiService.getData(pathname)
-//     return data
-//   }
-
-//   static async uploadFiles(endpoint, formData) {
-//     const url = new URL(endpoint, `${ROUTES.BASE_URL}:${ROUTES.PORT}`)
-//     const response = await axios.post(url.toString(), formData, {
-//       headers: { 'Content-Type': 'multipart/form-data' },
-//     })
-//     return response
-//   }
-
-//   //**************** */
-//   static async exportAll() {
-//     const url = new URL('/api/export/all', `${ROUTES.BASE_URL}:${ROUTES.PORT}`)
-//     const response = await axios.get(url.toString())
-//     return response
-//   }
-
-//   static async exportSession(limit = 100) {
-//     const url = new URL(
-//       `/api/export/session?limit=${limit}`,
-//       `${ROUTES.BASE_URL}:${ROUTES.PORT}`
-//     )
-//     const response = await axios.get(url.toString())
-//     return response
-//   }
-
-//   // Добавлен метод для экспорта по датам
-//   static async exportByDateRange(startDate, endDate) {
-//     const url = new URL(
-//       '/api/export/daterange',
-//       `${ROUTES.BASE_URL}:${ROUTES.PORT}`
-//     )
-//     url.searchParams.set('startDate', startDate)
-//     url.searchParams.set('endDate', endDate)
-//     const response = await axios.get(url.toString())
-//     return response
-//   }
-
-//   static async exportRange(startIp, endIp) {
-//     const url = new URL(
-//       `/api/export/range?startIp=${startIp}&endIp=${endIp}`,
-//       `${ROUTES.BASE_URL}:${ROUTES.PORT}`
-//     )
-//     const response = await axios.get(url.toString())
-//     return response
-//   }
 //   //**************** */
 // }
 
