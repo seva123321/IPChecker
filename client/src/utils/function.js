c678d2365418f73ed795cf4e9e273883bfdf16a0
Seva <20211206nch@mail.ru>
2025-11-23 18:31:41 +0300

    fix: getPriorityDisplayName in functions utils

--- client/src/utils/function.js
+++ client/src/utils/function.js
@@ -1,6 +1,19 @@
 export const renderPortsData = (data) => {
-    return data
-      .sort((a, b) => a.port - b.port)
-      .map((item) => `${item.port} (${item.name})`)
-      .join(', ');
-  };
+  return data
+    .sort((a, b) => a.port - b.port)
+    .map((item) => `${item.port} (${item.name})`)
+    .join(', ')
+}
+
+export const getPriorityDisplayName = (priorityId) => {
+  switch (priorityId) {
+    case '1':
+      return 'Обычный'
+    case '2':
+      return 'Интересный'
+    case '3':
+      return 'Важный'
+    default:
+      return priority?.name || 'Неизвестно'
+  }
+}
