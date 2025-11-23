2583867c3cd2bd68938b8b90f8ed6e732bf9e383
Seva <20211206nch@mail.ru>
2025-11-23 18:29:54 +0300

    fix: Button with forwardRef

--- client/src/Button/Button.jsx
+++ client/src/Button/Button.jsx
@@ -1,32 +1,41 @@
+import { forwardRef } from 'react'
 import cn from './Button.module.scss'
 
-export function Button({
-  children,
-  onClick,
-  variant = 'primary', // primary, secondary, danger
-  size = 'medium', // small, medium, large
-  disabled = false,
-  className = '',
-  ...props
-}) {
-  const buttonClasses = [
-    cn.button,
-    cn[variant],
-    cn[size],
-    disabled && cn.disabled,
-    className,
-  ]
-    .filter(Boolean)
-    .join(' ')
+export const Button = forwardRef(
+  (
+    {
+      children,
+      onClick,
+      variant = 'primary', // primary, secondary, danger
+      size = 'medium', // small, medium, large
+      disabled = false,
+      className = '',
+      ...props
+    },
+    ref
+  ) => {
+    const buttonClasses = [
+      cn.button,
+      cn[variant],
+      cn[size],
+      disabled && cn.disabled,
+      className,
+    ]
+      .filter(Boolean)
+      .join(' ')
 
-  return (
-    <button
-      className={buttonClasses}
-      onClick={onClick}
-      disabled={disabled}
-      {...props}
-    >
-      {children}
-    </button>
-  )
-}
+    return (
+      <button
+        ref={ref}
+        className={buttonClasses}
+        onClick={onClick}
+        disabled={disabled}
+        {...props}
+      >
+        {children}
+      </button>
+    )
+  }
+)
+
+Button.displayName = 'Button'
