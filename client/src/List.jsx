5ed5e18d5028ca50015d150528c2462be1a5ba36
Seva <20211206nch@mail.ru>
2025-12-07 15:23:49 +0300

    fix: added items?.map in List

--- client/src/List.jsx
+++ client/src/List.jsx
@@ -1,7 +1,7 @@
 export function List({ items, render }) {
   return (
     <ul>
-      {items.map((item, index) => (
+      {items?.map((item, index) => (
         <li key={item.id || item.uid || index}>{render(item)}</li>
       ))}
     </ul>
