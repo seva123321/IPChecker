7f56978db5c49f809f82ded494e9aa584ee2e29c
Seva <20211206nch@mail.ru>
2025-12-07 15:25:02 +0300

    fix: added countries and files models

--- api/models/Host.js
+++ api/models/Host.js
@@ -37,9 +37,73 @@ const Host = sequelize.define('Host', {
       key: 'id'
     }
   },
+  country_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'countries',
+      key: 'id'
+    }
+  },
+  file_source_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'file_sources',
+      key: 'id'
+    }
+  },
 }, {
   tableName: 'hosts',
   timestamps: false,
 });
 
 export default Host;
+
+
+// без FileSource and Country
+// import { DataTypes } from 'sequelize';
+// import sequelize from '../db.js';
+
+// const Host = sequelize.define('Host', {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
+//   ip: {
+//     type: DataTypes.INET,
+//     allowNull: false,
+//     unique: true,
+//   },
+//   reachable: {
+//     type: DataTypes.BOOLEAN,
+//     allowNull: false,
+//     defaultValue: true,
+//   },
+//   updated_at: {
+//     type: DataTypes.DATE,
+//     defaultValue: DataTypes.NOW,
+//   },
+//   priority_id: {
+//     type: DataTypes.INTEGER,
+//     allowNull: true,
+//     references: {
+//       model: 'host_priorities',
+//       key: 'id'
+//     }
+//   },
+//   grouping_id: {
+//     type: DataTypes.INTEGER,
+//     allowNull: true,
+//     references: {
+//       model: 'host_groupings',
+//       key: 'id'
+//     }
+//   },
+// }, {
+//   tableName: 'hosts',
+//   timestamps: false,
+// });
+
+// export default Host;
--- api/models/index.js
+++ api/models/index.js
@@ -6,6 +6,8 @@ import Whois from './Whois.js';
 import Priority from './Priority.js';
 import PriorityComment from './PriorityComment.js';
 import Grouping from './Grouping.js';
+import Country from './Country.js';
+import FileSource from './FileSource.js';
 import sequelize from '../db.js';
 
 // Связи
@@ -32,6 +34,13 @@ Priority.hasMany(PriorityComment, { foreignKey: 'priority_id' });
 Host.belongsTo(Grouping, { foreignKey: 'grouping_id' });
 Grouping.hasMany(Host, { foreignKey: 'grouping_id' });
 
+// Новые связи для стран и файлов
+Host.belongsTo(Country, { foreignKey: 'country_id' });
+Country.hasMany(Host, { foreignKey: 'country_id' });
+
+Host.belongsTo(FileSource, { foreignKey: 'file_source_id' });
+FileSource.hasMany(Host, { foreignKey: 'file_source_id' });
+
 // Экспорт
 const models = {
   Host,
@@ -42,6 +51,8 @@ const models = {
   Priority,
   PriorityComment,
   Grouping,
+  Country,
+  FileSource,
 };
 
 export {
@@ -53,6 +64,68 @@ export {
   Priority,
   PriorityComment,
   Grouping,
+  Country,
+  FileSource,
   sequelize,
   models,
 };
+
+
+// import Host from './Host.js';
+// import Port from './Port.js';
+// import WellKnownPort from './WellKnownPort.js';
+// import WhoisKey from './WhoisKey.js';
+// import Whois from './Whois.js';
+// import Priority from './Priority.js';
+// import PriorityComment from './PriorityComment.js';
+// import Grouping from './Grouping.js';
+// import sequelize from '../db.js';
+
+// // Связи
+// Host.hasMany(Port, { foreignKey: 'host_id', onDelete: 'CASCADE' });
+// Port.belongsTo(Host, { foreignKey: 'host_id' });
+
+// Host.hasMany(Whois, { foreignKey: 'host_id', onDelete: 'CASCADE' });
+// Whois.belongsTo(Host, { foreignKey: 'host_id' });
+
+// Whois.belongsTo(WhoisKey, { foreignKey: 'key_id', onDelete: 'CASCADE' });
+// WhoisKey.hasMany(Whois, { foreignKey: 'key_id' });
+
+// // Связи для приоритетов
+// Host.belongsTo(Priority, { foreignKey: 'priority_id' });
+// Priority.hasMany(Host, { foreignKey: 'priority_id' });
+
+// // Связи для комментариев приоритетов
+// PriorityComment.belongsTo(Host, { foreignKey: 'host_id' });
+// Host.hasOne(PriorityComment, { foreignKey: 'host_id' });
+// PriorityComment.belongsTo(Priority, { foreignKey: 'priority_id' });
+// Priority.hasMany(PriorityComment, { foreignKey: 'priority_id' });
+
+// // Связи для группировки
+// Host.belongsTo(Grouping, { foreignKey: 'grouping_id' });
+// Grouping.hasMany(Host, { foreignKey: 'grouping_id' });
+
+// // Экспорт
+// const models = {
+//   Host,
+//   Port,
+//   WellKnownPort,
+//   WhoisKey,
+//   Whois,
+//   Priority,
+//   PriorityComment,
+//   Grouping,
+// };
+
+// export {
+//   Host,
+//   Port,
+//   WellKnownPort,
+//   WhoisKey,
+//   Whois,
+//   Priority,
+//   PriorityComment,
+//   Grouping,
+//   sequelize,
+//   models,
+// };
