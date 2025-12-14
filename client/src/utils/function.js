25ac62a181be99719b5469a2694d393bc53f9f5d
Seva <20211206nch@mail.ru>
2025-12-14 17:47:23 +0300

    fix: client/src/utils/function.js

--- client/src/utils/function.js
+++ client/src/utils/function.js
@@ -17,3 +17,37 @@ export const getPriorityDisplayName = (priorityId) => {
       return priority?.name || 'Неизвестно'
   }
 }
+
+// Функция для кодирования имени файла с сохранением кириллицы
+export const encodeFileName = (fileName) => {
+  // Сохраняем расширение файла
+  const extension = fileName.split('.').pop()
+  const nameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'))
+
+  // Кодируем имя файла для передачи, сохраняя кириллицу
+  const encodedName = encodeURIComponent(nameWithoutExtension)
+
+  // Возвращаем имя с оригинальным расширением
+  return `${encodedName}.${extension}`
+}
+
+// Функция для декодирования имени файла
+export const decodeFileName = (fileName) => {
+  try {
+    // Разделяем имя и расширение
+    const extension = fileName.split('.').pop()
+    const encodedName = fileName.substring(0, fileName.lastIndexOf('.'))
+
+    // Декодируем имя файла
+    const decodedName = decodeURIComponent(encodedName)
+
+    return `${decodedName}.${extension}`
+  } catch (error) {
+    console.warn('Ошибка декодирования имени файла:', error)
+    return fileName // Возвращаем оригинальное имя в случае ошибки
+  }
+}
+
+export const generateClientId = () => {
+  return `client_${Date.now()}_${Math.random().toString(36).slice(2, 9)}`
+}
