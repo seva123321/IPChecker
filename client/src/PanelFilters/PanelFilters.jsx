15d3102ace98d68df736ebd2e66435c8962abb53
Seva <20211206nch@mail.ru>
2026-01-24 20:54:42 +0300

    fix: PanelFilters

--- client/src/PanelFilters/PanelFilters.jsx
+++ client/src/PanelFilters/PanelFilters.jsx
@@ -1,73 +1,22 @@
-import React, { useState, useCallback } from 'react'
 import { Input } from '../Input/Input'
 import { Button } from '../Button/Button'
 import cn from './PanelFilters.module.scss'
 import { SingleSelectDataList } from '../MultiSelectDataList'
 import { Checkbox, Tooltip } from 'antd'
 import { ClearOutlined } from '@ant-design/icons'
+import useSearch from './hooks/useSearch'
 
-const initialSearchText = {
-  ip: '',
-  port: '',
-  keyword: '',
-  portOpened: true,
-  portFiltered: false,
-}
-
-export function PanelFilters({ onSearch, onGroup, service }) {
-  const [searchText, setSearchText] = useState(initialSearchText)
-
-  // Оптимизированные обработчики с использованием useCallback
-  const updateField = useCallback((field, value) => {
-    setSearchText((prev) => ({ ...prev, [field]: value }))
-  }, [])
-
-  const clearField = useCallback(
-    (field) => {
-      updateField(field, '')
-    },
-    [updateField]
-  )
-
-  const handleClearAll = useCallback(() => {
-    setSearchText(initialSearchText)
-    onSearch('ip', {})
-  }, [onSearch])
-
-  const handlePortChange = useCallback(
-    (value) => {
-      // Извлекаем только номер порта из строки "53 (dns)"
-      const portNumber = value.split(' ')[0]
-      updateField('port', portNumber)
-    },
-    [updateField]
-  )
-
-  const handleKeywordInputChange = useCallback(
-    (e) => {
-      updateField('keyword', e.target.value)
-    },
-    [updateField]
-  )
-
-  const getPortPrepared = useCallback(
-    (data) => data?.data?.map((el) => `${el.port} (${el.name})`) || [],
-    []
-  )
-
-  const handlePortOpenedChange = useCallback(
-    (e) => {
-      updateField('portOpened', e.target.checked)
-    },
-    [updateField]
-  )
-
-  const handlePortFilteredChange = useCallback(
-    (e) => {
-      updateField('portFiltered', e.target.checked)
-    },
-    [updateField]
-  )
+export function PanelFilters({ onSearch, onGroup, service, setSearchParams }) {
+  const {
+    searchText,
+    clearField,
+    updateField,
+    handleClearAll,
+    getPortPrepared,
+    handlePortChange,
+    handleKeywordChange,
+    handleCheckboxChange,
+  } = useSearch({ onSearch, setSearchParams })
 
   return (
     <div className={cn.panel}>
@@ -121,7 +70,7 @@ export function PanelFilters({ onSearch, onGroup, service }) {
                 <label className={cn.checkboxLabel}>
                   <Checkbox
                     checked={searchText.portOpened}
-                    onChange={handlePortOpenedChange}
+                    onChange={(e) => handleCheckboxChange('portOpened', e)}
                   />
                   Открытые
                 </label>
@@ -130,7 +79,7 @@ export function PanelFilters({ onSearch, onGroup, service }) {
                 <label className={cn.checkboxLabel}>
                   <Checkbox
                     checked={searchText.portFiltered}
-                    onChange={handlePortFilteredChange}
+                    onChange={(e) => handleCheckboxChange('portFiltered', e)}
                   />
                   Фильтрованные
                 </label>
@@ -142,8 +91,8 @@ export function PanelFilters({ onSearch, onGroup, service }) {
             onClick={() =>
               onSearch('ports', {
                 port: searchText.port,
-                o: searchText.portOpened,
-                f: searchText.portFiltered,
+                portOpened: searchText.portOpened,
+                portFiltered: searchText.portFiltered,
               })
             }
             className={cn.searchButton}
@@ -153,9 +102,10 @@ export function PanelFilters({ onSearch, onGroup, service }) {
           </Button>
           <Button
             onClick={() =>
-              onGroup('ports/group', {
-                o: searchText.portOpened,
-                f: searchText.portFiltered,
+              onSearch('ports/group', {
+                // port: '',
+                portOpened: searchText.portOpened,
+                portFiltered: searchText.portFiltered,
               })
             }
             // onClick={() => onGroup('ports/group')}
@@ -169,7 +119,7 @@ export function PanelFilters({ onSearch, onGroup, service }) {
             value={searchText.keyword}
             placeholder="Поиск по ключевым словам"
             containerClass={{ width: 300 }}
-            onChange={handleKeywordInputChange}
+            onChange={handleKeywordChange}
             showClear
             onClear={() => clearField('keyword')}
           />
@@ -183,7 +133,8 @@ export function PanelFilters({ onSearch, onGroup, service }) {
             Найти
           </Button>
           <Button
-            onClick={() => onGroup('keywords/group',{})}
+            onClick={() => onSearch('keywords/group', {})}
+            // onClick={() => onGroup('keywords/group', {})}
             className={cn.groupButton}
           >
             Группировать
@@ -193,3 +144,194 @@ export function PanelFilters({ onSearch, onGroup, service }) {
     </div>
   )
 }
+// import React, { useState, useCallback } from 'react'
+// import { Input } from '../Input/Input'
+// import { Button } from '../Button/Button'
+// import cn from './PanelFilters.module.scss'
+// import { SingleSelectDataList } from '../MultiSelectDataList'
+// import { Checkbox, Tooltip } from 'antd'
+// import { ClearOutlined } from '@ant-design/icons'
+// import { initialSearchText } from '../utils/constant'
+
+// export function PanelFilters({ onSearch, onGroup, service, setSearchParams}) {
+//   const [searchText, setSearchText] = useState(initialSearchText)
+
+//   // Оптимизированные обработчики с использованием useCallback
+//   const updateField = useCallback((field, value) => {
+//     setSearchText((prev) => ({ ...prev, [field]: value }))
+//     setSearchParams((prev) => ({ ...prev, [field]: value }))
+//   }, [])
+
+//   const clearField = useCallback(
+//     (field) => {
+//       updateField(field, '')
+//     },
+//     [updateField]
+//   )
+
+//   const handleClearAll = useCallback(() => {
+//     setSearchText(initialSearchText)
+//     setSearchParams(initialSearchText)
+//     onSearch('ip', {})
+//   }, [onSearch])
+
+//   const handlePortChange = useCallback(
+//     (value) => {
+//       // Извлекаем только номер порта из строки "53 (dns)"
+//       const portNumber = value.split(' ')[0]
+//       updateField('port', portNumber)
+//     },
+//     [updateField]
+//   )
+
+//   const handleKeywordInputChange = useCallback(
+//     (e) => {
+//       updateField('keyword', e.target.value)
+//     },
+//     [updateField]
+//   )
+
+//   const getPortPrepared = useCallback(
+//     (data) => data?.data?.map((el) => `${el.port} (${el.name})`) || [],
+//     []
+//   )
+
+//   const handlePortOpenedChange = useCallback(
+//     (e) => {
+//       updateField('portOpened', e.target.checked)
+//     },
+//     [updateField]
+//   )
+
+//   const handlePortFilteredChange = useCallback(
+//     (e) => {
+//       updateField('portFiltered', e.target.checked)
+//     },
+//     [updateField]
+//   )
+
+//   return (
+//     <div className={cn.panel}>
+//       <div className={cn.actionsGroup}>
+//         <h2>Введите значение</h2>
+//         <Tooltip title="Сбросить все поля">
+//           <Button onClick={handleClearAll} className={cn.clearAllButton}>
+//             <ClearOutlined />
+//           </Button>
+//         </Tooltip>
+//       </div>
+//       <ul className={cn.list}>
+//         <li className={cn.searchGroup}>
+//           <Input
+//             value={searchText.ip}
+//             placeholder="Поиск по IP"
+//             containerClass={{ width: 300 }}
+//             onChange={(e) => updateField('ip', e.target.value)}
+//             showClear
+//             onClear={() => clearField('ip')}
+//           />
+//           <Button
+//             onClick={() => onSearch('ip', { ip: searchText.ip })}
+//             className={cn.searchButton}
+//             disabled={!searchText.ip.trim()}
+//           >
+//             Найти
+//           </Button>
+//           <Button
+//             // onClick={() => onGroup('ip/group')}
+//             onClick={() => onSearch('ip')}
+//             className={cn.groupButton}
+//           >
+//             Группировать
+//           </Button>
+//         </li>
+//         <li className={cn.searchGroup}>
+//           <div className={cn.portInputsContainer}>
+//             <SingleSelectDataList
+//               service={service}
+//               value={searchText.port}
+//               onChange={handlePortChange}
+//               placeholder="Поиск по портам"
+//               fetchDataUrl="data"
+//               fetchParams={{ q: 'port' }}
+//               getDataPrepared={getPortPrepared}
+//               dataListId="port"
+//             />
+//             <div className={cn.checkboxContainer}>
+//               <Tooltip title="Открытые порты">
+//                 <label className={cn.checkboxLabel}>
+//                   <Checkbox
+//                     checked={searchText.portOpened}
+//                     onChange={handlePortOpenedChange}
+//                   />
+//                   Открытые
+//                 </label>
+//               </Tooltip>
+//               <Tooltip title="Фильтрованные порты">
+//                 <label className={cn.checkboxLabel}>
+//                   <Checkbox
+//                     checked={searchText.portFiltered}
+//                     onChange={handlePortFilteredChange}
+//                   />
+//                   Фильтрованные
+//                 </label>
+//               </Tooltip>
+//             </div>
+//           </div>
+
+//           <Button
+//             onClick={() =>
+//               onSearch('ports', {
+//                 port: searchText.port,
+//                 portOpened: searchText.portOpened,
+//                 portFiltered: searchText.portFiltered,
+//               })
+//             }
+//             className={cn.searchButton}
+//             disabled={!searchText.port.trim()}
+//           >
+//             Найти
+//           </Button>
+//           <Button
+//                onClick={() =>
+//               onSearch('ports/group', {
+//                 // port: '',
+//                 portOpened: searchText.portOpened,
+//                 portFiltered: searchText.portFiltered,
+//               })
+//             }
+//             // onClick={() => onGroup('ports/group')}
+//             className={cn.groupButton}
+//           >
+//             Группировать
+//           </Button>
+//         </li>
+//         <li className={cn.searchGroup}>
+//           <Input
+//             value={searchText.keyword}
+//             placeholder="Поиск по ключевым словам"
+//             containerClass={{ width: 300 }}
+//             onChange={handleKeywordInputChange}
+//             showClear
+//             onClear={() => clearField('keyword')}
+//           />
+//           <Button
+//             onClick={() =>
+//               onSearch('keywords', { keyword: searchText.keyword })
+//             }
+//             className={cn.searchButton}
+//             disabled={!searchText.keyword.trim()}
+//           >
+//             Найти
+//           </Button>
+//           <Button
+//             onClick={() => onGroup('keywords/group',{})}
+//             className={cn.groupButton}
+//           >
+//             Группировать
+//           </Button>
+//         </li>
+//       </ul>
+//     </div>
+//   )
+// }
